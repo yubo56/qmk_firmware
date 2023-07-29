048e224340c2263cb242b225797b487c6e991afa
Yubo Su <yubo56@gmail.com>
2023-07-29 01:58:02 -0400

    keep a small bit of debounce

--- keyboards/xiudi/xd75/config.h
+++ keyboards/xiudi/xd75/config.h
@@ -40,7 +40,7 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 //#    define RGBLIGHT_EFFECT_BREATHE_MAX    255   // 0 to 255
 
 /* Debounce reduces chatter (unintended double-presses) - set 0 if debouncing is not needed */
-#define DEBOUNCE 0
+#define DEBOUNCE 3
 
 /* define if matrix has ghost (lacks anti-ghosting diodes) */
 //#define MATRIX_HAS_GHOST
