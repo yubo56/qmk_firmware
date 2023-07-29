357c540299f97e3cbdbf5283e991072ebc015312
Yubo Su <yubo56@gmail.com>
2023-07-29 01:43:25 -0400

    updated/reformatted keymap

--- keyboards/xiudi/xd75/rules.mk
+++ keyboards/xiudi/xd75/rules.mk
@@ -7,7 +7,7 @@ EXTRAKEY_ENABLE = yes       # Audio control and System control
 CONSOLE_ENABLE = no         # Console for debug
 COMMAND_ENABLE = yes        # Commands for debug and configuration
 NKRO_ENABLE = yes           # Enable N-Key Rollover
-BACKLIGHT_ENABLE = no       # Enable keyboard backlight functionality
+BACKLIGHT_ENABLE = yes      # Enable keyboard backlight functionality
 RGBLIGHT_ENABLE = yes       # Enable keyboard RGB underglow
 AUDIO_ENABLE = no           # Audio output
 
