e5d075dad7ff5fd09742078454589538c7d45c1a
Yubo Su <yubo56@gmail.com>
2024-01-23 11:19:46 +0100

    no need to keep xd75 and iris keymaps compatible

--- keyboards/xiudi/xd75/keymaps/yubo/keymap.c
+++ keyboards/xiudi/xd75/keymaps/yubo/keymap.c
@@ -27,7 +27,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_TAB           , KC_Q    , KC_W    , KC_E    , KC_R    , KC_T    , KC_P7   , KC_P8   , KC_P9   , KC_Y    , KC_U    , KC_I    , KC_O    , KC_P    , KC_BSLS ,
             MO(1)            , KC_A    , KC_S    , KC_D    , KC_F    , KC_G    , KC_P4   , KC_P5   , KC_P6   , KC_H    , KC_J    , KC_K    , KC_L    , KC_SCLN , KC_ENT  ,
             KC_LSFT          , KC_Z    , KC_X    , KC_C    , KC_V    , KC_B    , KC_P1   , KC_P2   , KC_P3   , KC_N    , KC_M    , KC_COMM , KC_DOT  , KC_SLSH , KC_RSFT ,
-            KC_TRNS          , KC_TRNS , KC_TRNS , MO(2)   , KC_LGUI , KC_SPC  , KC_LEFT , KC_P0   , KC_RGHT , KC_TRNS , KC_LCTL , KC_LALT , KC_VOLD , KC_VOLU , TG(3)
+            KC_TRNS          , KC_TRNS , KC_TRNS , MO(2)   , KC_LGUI , KC_SPC  , KC_LEFT , KC_P0   , KC_RGHT , KC_LCTL , KC_LALT , KC_TRNS , KC_VOLD , KC_VOLU , TG(3)
             )                ,
 	[B] = LAYOUT_ortho_5x15(
             /* 1             , 2       , 3       , 4       , 5       , 6       , 7       , 8       , 9       , 10      , 11      , 12      , 13      , 14      , 15      , */
@@ -43,7 +43,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_TRNS          , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , RGB_TOG , RGB_MOD , RGB_RMOD, KC_TRNS , KC_TRNS , KC_TRNS , KC_F11  , KC_F12  , KC_TRNS ,
             KC_TRNS          , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , RGB_HUI , RGB_HUD , RGB_SAI , RGB_SAD , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,
             KC_TRNS          , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , RGB_VAI , RGB_VAD , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS ,
-            KC_TRNS          , KC_TRNS , KC_TRNS , KC_TRNS , KC_LGUI , KC_LCTL , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS , KC_LCTL , KC_LALT , KC_TRNS , KC_TRNS , KC_TRNS
+            KC_TRNS          , KC_TRNS , KC_TRNS , KC_TRNS , KC_LGUI , KC_LCTL , KC_TRNS , KC_TRNS , KC_TRNS , KC_LCTL , KC_LALT , KC_TRNS , KC_TRNS , KC_TRNS , KC_TRNS
             )                ,
 	[D] = LAYOUT_ortho_5x15(
             /* 1             , 2       , 3       , 4       , 5       , 6       , 7       , 8       , 9       , 10      , 11      , 12      , 13      , 14      , 15      , */
@@ -51,7 +51,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
             KC_TAB           , KC_QUOT , KC_COMM , KC_DOT  , KC_P    , KC_Y    , KC_P7   , KC_P8   , KC_P9   , KC_F    , KC_G    , KC_C    , KC_R    , KC_L    , KC_BSLS ,
             MO(4)            , KC_A    , KC_O    , KC_E    , KC_U    , KC_I    , KC_P4   , KC_P5   , KC_P6   , KC_D    , KC_H    , KC_T    , KC_N    , KC_S    , KC_ENT  ,
             KC_LSFT          , KC_SCLN , KC_Q    , KC_J    , KC_K    , KC_X    , KC_P1   , KC_P2   , KC_P3   , KC_B    , KC_M    , KC_W    , KC_V    , KC_Z    , KC_RSFT ,
-            KC_TRNS          , KC_TRNS , KC_TRNS , MO(4)   , KC_LGUI , KC_SPC  , KC_LEFT , KC_P0   , KC_RGHT , KC_TRNS , KC_RCTL , KC_RALT , KC_VOLD , KC_VOLU , KC_TRNS
+            KC_TRNS          , KC_TRNS , KC_TRNS , MO(4)   , KC_LGUI , KC_SPC  , KC_LEFT , KC_P0   , KC_RGHT , KC_RCTL , KC_RALT , KC_TRNS , KC_VOLD , KC_VOLU , KC_TRNS
             )                ,
 	[E] = LAYOUT_ortho_5x15(
             /* 1             , 2       , 3       , 4       , 5       , 6       , 7       , 8       , 9       , 10      , 11      , 12      , 13      , 14      , 15      , */
